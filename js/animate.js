f449aef2c85629bed94c211d11016345853f2494
Stephen L. Fox <SFox@WellviewHealth.com>
2023-09-02 12:21:31 -0500

    Services.html Page update Moving to: - EmailJS configuration - Case Study

--- js/animate.js
+++ js/animate.js
@@ -2,7 +2,7 @@ $('document').ready(function(){
     let bodyFaded = $('body');
     setTimeout(function(){
         $(bodyFaded).css('opacity','1.0');
-    },1000)
+    },500)
     let serviceIcon = '.service-icon';
     setTimeout(function(){
         $('.service-icon.animated-icon').removeClass('animated-icon')
@@ -19,15 +19,25 @@ $(window).scroll(function (event) {
     for (let ar = 0; ar < $(animatedRight).length; ar++){
         var animatedPosition = $((animatedRight[ar])).offset().top
         console.log(scroll,animatedPosition)
-        animatedPosition / 2 < scroll ?  $(animatedRight[ar]).removeClass('animated-slide-right').removeClass('animated-slide-left')  : "";
+        animatedPosition / 3 < scroll ?  $(animatedRight[ar]).removeClass('animated-slide-right').removeClass('animated-slide-left')  : "";
     }
 
     for (let ar = 0; ar < $(animatedRight).length; ar++){
         var animatedPosition = $((animatedLeft[ar])).offset().top
-        animatedPosition / 2 < scroll ?  $(animatedLeft[ar]).removeClass('animated-slide-right').removeClass('animated-slide-left')  : "";
+        animatedPosition / 3 < scroll ?  $(animatedLeft[ar]).removeClass('animated-slide-right').removeClass('animated-slide-left')  : "";
     }
 });
 
 $('#scheduleMeetingFirst').on('click', () => {
     $('#scheduleModal').modal('hide');
 })
+
+ // Add slideDown animation to Bootstrap dropdown when expanding.
+ $('.dropdown').on('show.bs.dropdown', function() {
+    $(this).find('.dropdown-menu').first().slideDown();
+  });
+
+  // Add slideUp animation to Bootstrap dropdown when collapsing.
+  $('.dropdown').on('hide.bs.dropdown', function() {
+    $(this).find('.dropdown-menu').first().slideUp();
+  });
