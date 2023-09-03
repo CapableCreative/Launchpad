596295461bb895bbad57284797814125b6000896
Stephen L. Fox <SFox@WellviewHealth.com>
2023-09-03 12:45:03 -0500

    Mobile Considerations

--- js/animate.js
+++ js/animate.js
@@ -9,21 +9,42 @@ $('document').ready(function(){
     }, 2000)  
 })
 
-$(window).scroll(function (event) {
-    var scroll = $(window).scrollTop();
-    var animatedRight = $('.animate-me.animated-slide-right');
-    var animatedLeft = $('.animate-me.animated-slide-left');
 
-    for (let ar = 0; ar < $(animatedRight).length; ar++){
-        var animatedPosition = $((animatedRight[ar])).offset().top
-        //console.log(scroll,animatedPosition)
-        animatedPosition / 3 < scroll ?  $(animatedRight[ar]).removeClass('animated-slide-right').removeClass('animated-slide-left')  : "";
-    }
 
-    for (let ar = 0; ar < $(animatedRight).length; ar++){
-        var animatedPosition = $((animatedLeft[ar])).offset().top
-        animatedPosition / 3 < scroll ?  $(animatedLeft[ar]).removeClass('animated-slide-right').removeClass('animated-slide-left')  : "";
+jQuery(document).ready(function(){
+    function resizeForm(){
+        var width = (window.innerWidth > 0) ? window.innerWidth : document.documentElement.clientWidth;
+        var animatedRight = $('.animate-me.animated-slide-right');
+        var animatedLeft = $('.animate-me.animated-slide-left');
+        if(width > 1024){
+            $(window).scroll(function (event) {
+                var scroll = $(window).scrollTop();
+            
+                for (let ar = 0; ar < $(animatedRight).length; ar++){
+                    var animatedPosition = $((animatedRight[ar])).offset().top
+                    //console.log(scroll,animatedPosition)
+                    animatedPosition / 3 < scroll ?  $(animatedRight[ar]).removeClass('animated-slide-right').removeClass('animated-slide-left')  : "";
+                }
+            
+                for (let ar = 0; ar < $(animatedRight).length; ar++){
+                    var animatedPosition = $((animatedLeft[ar])).offset().top
+                    animatedPosition / 3 < scroll ?  $(animatedLeft[ar]).removeClass('animated-slide-right').removeClass('animated-slide-left')  : "";
+                }
+            });
+        } 
+        else {
+            for (let ar = 0; ar < $(animatedRight).length; ar++){
+                var animatedPosition = $((animatedRight[ar])).offset().top
+                $(animatedRight[ar]).removeClass('animated-slide-right').removeClass('animated-slide-left');
+            }
+        
+            for (let ar = 0; ar < $(animatedRight).length; ar++){
+                $(animatedLeft[ar]).removeClass('animated-slide-right').removeClass('animated-slide-left');
+            }
+        }  
     }
+    window.onresize = resizeForm;
+    resizeForm();
 });
 
 $('#scheduleMeetingFirst').on('click', () => {
