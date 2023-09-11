b5bd651659f950ac076ced0f0e5a0641fe0a1885
Stephen L. Fox <SFox@WellviewHealth.com>
2023-09-11 13:16:22 -0500

    ERROR AND SUCCESS HANDLING contact form

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,10 @@
 window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
+
+        let contact = $("#contactWrap");
+        let contactSuccess = $("#contactSuccess");
+        let contactSend = $("#contactSend");
+                
         event.preventDefault();
         // generate a five digit number for the contact_number variable
         this.contact_number.value = Math.random() * 100000 | 0;
@@ -7,25 +12,19 @@ window.onload = function() {
         emailjs.sendForm('service_y8tdj6b', 'template_ck00kvj', this)
             .then(function() {
                 console.log('SUCCESS!');
-                document.getElementById("contact-form").reset()
-                let contact = $("#contactWrap");
-                let contactSuccess = $("#contactSuccess");
-                let contactSend = $("#contactSend");
+                document.getElementById("contact-form").reset()        
                 $(contact).fadeOut();
                 $(contactSend).fadeOut();
                 $(contactSuccess).fadeIn();
             }, function(error) {
                 console.log('FAILED...', error);
                 $("#contactWrap").prepend('<div class="error-box">There seems to be an issue with this contact form. Please email us directly at launch@launchpaddevs.com.</div>');
-                setTimeout(
-                    () => {
-                        $('#contactModal').modal('hide');
-                        $(".error-box").remove()
-                    }, 5000
-                )
+                $(contactSend).fadeOut();
             });
     });
 }
 $('#contactModal').on('hidden.bs.modal', function () {
     $(this).find('form').trigger('reset');
+    $(".error-box").remove();
+    $(contactSend).show();
 })
